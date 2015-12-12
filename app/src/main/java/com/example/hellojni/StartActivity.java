0f4f79db0ad9f20ba3a35d381fa69eddb9868d1f
German <georg.88@mail.ru>
2015-12-12 17:27:03 +0600

    add dictionary

--- app/src/main/java/com/example/hellojni/StartActivity.java
+++ app/src/main/java/com/example/hellojni/StartActivity.java
@@ -1,11 +1,30 @@
 package com.example.hellojni;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class StartActivity extends Activity {
+
+
+    public static String LOG_TAG = "my_log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +32,91 @@ public class StartActivity extends Activity {
         setContentView(R.layout.activity_start);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start, menu);
-        return true;
+    public void getTranslate(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        new ParseTask().execute(editText.getText().toString());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class ParseTask extends AsyncTask<String, Void, String> {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        @Override
+        protected String doInBackground(String... params) {
+            // получаем данные с внешнего ресурса
+            try {
+                String key = "dict.1.1.20151211T161559Z.76ec6129fecd755b.447faf7495f92ff3f651ec3928e36d8cbacefb23";
+                String word = URLEncoder.encode(params[0], "UTF-8"); //"time";
+                URL url = new URL("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=" + key + "&lang=ru-ru&text=" + word);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+//            return "hello";
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // выводим целиком полученную json-строку
+
+            TextView textView = (TextView) findViewById(R.id.txtTranslate);
+            textView.setText(strJson);
+
+//            Log.d(LOG_TAG, strJson);
+//
+//            JSONObject dataJsonObj = null;
+//            String secondName = "";
+
+//            try {
+//                dataJsonObj = new JSONObject(strJson);
+//                JSONArray friends = dataJsonObj.getJSONArray("friends");
+//
+//                // 1. достаем инфо о втором друге - индекс 1
+//                JSONObject secondFriend = friends.getJSONObject(1);
+//                secondName = secondFriend.getString("name");
+//                Log.d(LOG_TAG, "Второе имя: " + secondName);
+//
+//                // 2. перебираем и выводим контакты каждого друга
+//                for (int i = 0; i < friends.length(); i++) {
+//                    JSONObject friend = friends.getJSONObject(i);
+//
+//                    JSONObject contacts = friend.getJSONObject("contacts");
+//
+//                    String phone = contacts.getString("mobile");
+//                    String email = contacts.getString("email");
+//                    String skype = contacts.getString("skype");
+//
+//                    Log.d(LOG_TAG, "phone: " + phone);
+//                    Log.d(LOG_TAG, "email: " + email);
+//                    Log.d(LOG_TAG, "skype: " + skype);
+//                }
+//
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
     }
+
 }
