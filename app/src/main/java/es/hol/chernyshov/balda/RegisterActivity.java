9ca986f4f4f9b64ff1ef183976595b085f7bc264
Your Name <georg.88@mail.ru>
2016-01-05 18:59:44 +0600

    edit login and registration

--- app/src/main/java/es/hol/chernyshov/balda/RegisterActivity.java
+++ app/src/main/java/es/hol/chernyshov/balda/RegisterActivity.java
@@ -2,7 +2,25 @@ package es.hol.chernyshov.balda;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RegisterActivity extends Activity {
     private SharedPreferences mSettings;
@@ -11,5 +29,81 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+    }
+
+    public void registration(View view) {
+        RegistrationTask registrationTask = new RegistrationTask();
+        registrationTask.execute();
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    private class RegistrationTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpURLConnection urlConnection = null;
+            //JSONArray jsonRecords = null;
+            String resultJson = "";
+            try {
+                URL url = new URL("http://chernyshov.hol.es/user");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+                urlConnection.setRequestMethod("POST");
+                OutputStream outputStream = urlConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                JSONObject data = new JSONObject();
+                JSONObject user = new JSONObject();
+                data.put("username", "Max");
+                data.put("password", "pass");
+                Log.d("BaldaNDK", data.toString());
+                writer.write(data.toString());
+                writer.flush();
+                writer.close();
+                outputStream.close();
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                resultJson = buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
+            }
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+            Log.d("BaldaNDK", data);
+
+            try {
+                JSONObject jsonObject = new JSONObject(data);
+                if (jsonObject.has("code")) {
+                    if (jsonObject.getInt("code") == 1) {
+                        String text = getResources().getString(R.string.error_server);
+                        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+                        toast.show();
+                    } else if (jsonObject.getInt("code") == 0) { // TODO код когда юзер существует
+
+                    }
+                } else {
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
